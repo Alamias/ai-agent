9af85653f4bdcd1de781760244b92afb22e2f39e
Tim Cox <alamias@cox.net>
2026-05-10 15:06:42 -0700

    wip

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 load_dotenv()
 
@@ -31,6 +32,7 @@ def main() -> None:
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
 
     if args.verbose:
